2d835b35fc4e87a5b646ee526466481bb76c98b0
UTxSHADOW <utkrishttech.official@gmail.com>
2020-02-18 10:21:56 +0530

    Major Update

---
+++ src/CLIClient/manager.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"tools"
+)
+
+func start() {
+	fmt.Println(tools.GetArg())
+}
---
+++ src/tools/backend.go
@@ -0,0 +1,20 @@
+package tools
+
+import (
+	"fmt"
+	"os"
+)
+
+// GetArg : Collects Command Line Arguments and returns them
+func GetArg() string {
+	switch l := len(os.Args); l {
+	case 1:
+		return "charge"
+	case 2:
+		return os.Args[1]
+	default:
+		fmt.Println("Usage Error: Too Many Arguments, use 'electrix help' instead.")
+		os.Exit(1)
+	}
+	return "nil"
+}
